4a7e94195700f7057f29382677e3b9fd08ad4315
kart1ka <kartik8942@gmail.com>
2021-03-23 00:33:32 +0530

    Promise part-2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DISHES } from '../shared/dishes';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 
@@ -11,15 +12,24 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   getLeader(id: string): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]); 
+    return new Promise(resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+    }); 
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured) [0]);
+    return new Promise (resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured) [0]), 2000);
+    });
   }
 
 }
